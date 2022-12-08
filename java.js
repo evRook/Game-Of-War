4fb0c9940f77b8c4305b3e33a76821df137c510f
Eric_Spychalski <EricSpychalski@gmail.COM>
2022-12-08 14:38:25 -0700

    deal cards function

--- java.js
+++ java.js
@@ -2,11 +2,11 @@
 
 var deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14];
 
-// var playerCards = []
+var playerDeck = [];
+
+var compDeck = [];
 
 // var playerWon = []
-
-// var compCards = []
 
 // var compWon = []
 
@@ -24,4 +24,17 @@ function shuffle() {
 }
 
 shuffle();
-console.log(deck);
+
+function dealCards(){
+    for(k=deck.length-1; k>=0; k--){
+        if(k % 2 === 0){
+            playerDeck.unshift(deck.splice(k,1)[0]);
+        }else if(k % 2 === 1){
+            compDeck.unshift(deck.splice(k,1)[0]);
+        }
+    }
+}
+
+dealCards();
+console.log(playerDeck);
+console.log(compDeck);
